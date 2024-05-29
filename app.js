6b74bdfe8db7d761116d41bd0409bbd5a08fa95e
Ruben <r.moronhinojosa@gmail.com>
2024-05-29 20:24:18 +0200

    change cors

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const app = express()
 const server = http.createServer(app)
 const io =  new SocketServer(server, {
     cors:{
-        origin:'http://localhost:5173' 
+        origin:'http://localhost:5173' ,
+        origin:'https://rmpadelindoor.netlify.app'
     }
 })
 
